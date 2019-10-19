869bee1642692ffc0dac3648d74ea557721b014e
Chris Manning <cmanning999@gmail.com>
2019-10-20 00:24:58 +0100

    Remove type level fmap

--- melo-format/src/Melo/Format/Internal/TypeLevel.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE TypeInType #-}
-
-module Melo.Format.Internal.TypeLevel where
-
-type family FMap (m :: a -> b) (x :: f a) :: f b
-
-type instance FMap m '[] = '[]
-
-type instance FMap m (v ': vs) = m v ': FMap m vs
